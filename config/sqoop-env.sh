883fd429eafae5d2e280b0641b249660ef14a3a8
Trung Tran <tutrung.leo@gmail.com>
2021-11-08 15:18:30 +0700

    config enviroment

---
+++ config/sqoop-env.sh
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env bash
+
+echo "sqoop env ingggggg"
+
+#-------------------------------------------
+#Set path to where bin/hadoop is available
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+
+#Set path to where hadoop-*-core.jar is available
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+
+#Set the path to where bin/hive is available
+export HIVE_HOME=$HIVE_HOME
+#-------------------------------------------
+# eof
